d8ee3db4899284f50f2db416ec34c7a37ca03ae3
Kaj Fehlhaber <75676366+kejne@users.noreply.github.com>
2025-09-08 11:32:56 +0200

    refactor: arrange mise tasks to clarify hierarchy (#31)
    Since mise users likely have global tasks, it would be useful to specify
    that tasks are indeed from the nauth project.
    
    The sub-tasks from the web page are also moved to align with this.
    
    Fixes: #28

--- .mise-tasks/nauth/generate-docs.sh
+++ .mise-tasks/nauth/generate-docs.sh
@@ -5,14 +5,14 @@
 crd-ref-docs --source-path=$MISE_PROJECT_ROOT/api --config=$MISE_PROJECT_ROOT/api/config.yaml --renderer=markdown --output-path=$MISE_PROJECT_ROOT/www/src/content/docs/crds.md
 
 # Add the required frontmatter for Starlight
-sed -i '' '1i\
+sed -i '1i\
 ---\
 title: API Reference\
 description: Complete API reference for nauth CRDs\
 ---\
-' $MISE_PROJECT_ROOT/www/src/content/docs/crds.md
+' "$MISE_PROJECT_ROOT/www/src/content/docs/crds.md"
 
 # Remove the first H1 header since Starlight will generate it from frontmatter
-sed -i '' '/^# API Reference$/d' $MISE_PROJECT_ROOT/www/src/content/docs/crds.md
+sed -i '/^# API Reference$/d' "$MISE_PROJECT_ROOT/www/src/content/docs/crds.md"
 
 helm-docs
