1db01813bf23e00fc9ad76621a2465fcf1af6cde
Illia Ovchynnikov <illia.ovchynnikov@gmail.com>
2025-12-01 22:17:22 +0100

    Remove unused RefreshState

--- internal/core/ports/in.go
+++ internal/core/ports/in.go
@@ -4,11 +4,9 @@ import (
 	"context"
 
 	natsv1alpha1 "github.com/WirelessCar/nauth/api/v1alpha1"
-	"github.com/WirelessCar/nauth/internal/core/domain/types"
 )
 
 type AccountManager interface {
-	RefreshState(ctx context.Context, observed *types.Account, desired *natsv1alpha1.Account) error
 	CreateAccount(ctx context.Context, state *natsv1alpha1.Account) error
 	UpdateAccount(ctx context.Context, state *natsv1alpha1.Account) error
 	DeleteAccount(ctx context.Context, desired *natsv1alpha1.Account) error
