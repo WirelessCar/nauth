8f10cbc3e29f05495e3c1fd94893f2bd7c03f11f
Subhangi <bhardwajsubhangi6@gmail.com>
2026-09-15 15:30:48 +0200

    chore: remove obsolete TODO
    Signed-off-by: Subhangi <bhardwajsubhangi6@gmail.com>

--- internal/adapter/inbound/controller/status.go
+++ internal/adapter/inbound/controller/status.go
@@ -49,7 +49,6 @@ func (s *statusReporter) status(ctx context.Context, object Object) (ctrl.Result
 	}
 
 	// Spreading out the requeue to avoid all being queued at the same time
-	// TODO: Make the status requeue interval configurable instead of hard-coding five minutes.
 	return ctrl.Result{
 		RequeueAfter: time.Duration(float64(5*time.Minute) * (0.9 + 0.2*rand.Float64())),
 	}, nil
